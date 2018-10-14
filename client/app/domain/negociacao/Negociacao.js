9b73ff20efbc06559fd06b09478cca1cca7390fc
Rogerio Teixeira <rogerioteixeira91@gmail.com>
2018-10-14 16:51:42 -0300

    2.11 Atalho para propriedades de objetos literais

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,9 +1,11 @@
 class Negociacao{
     
     constructor(data, quantidade, valor){
-        this._data = new Date(data.getTime());
-        this._quantidade = quantidade;
-        this._valor = valor;
+        Object.assign(this, {
+            _data: new Date(data.getTime()),
+            _quantidade: quantidade,
+            _valor: valor
+        });
         Object.freeze(this);
     }
 
