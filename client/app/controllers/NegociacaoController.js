f3c1d874f150a56166bac72da008bb5e2d6139ff
Rogerio Teixeira <rogerioteixeira91@gmail.com>
2018-10-14 17:55:58 -0300

    3.5 criando uma instancia de negociacao

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,11 +10,15 @@ class NegociacaoController {
     }
 
     adiciona(event){
-        event.preventDefault();        
+        event.preventDefault();
 
-        console.log(this._inputData.value);
-        console.log(parseInt(this._inputQuantidade.value));
-        console.log(parseFloat(this._inputValor.value));
+        let negociacao = new Negociacao(
+            this._inputData.value,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        )
+
+        console.log(negociacao);
 
     }
 }
